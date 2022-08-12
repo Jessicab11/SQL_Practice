5dc9d2ff88a50159a4cdbd297f0af50158eb38fc
Jessicab11 <79180025+Jessicab11@users.noreply.github.com>
2022-08-12 17:11:36 -0400

    Add notes to code

--- films_with_nick_wahlberg.sql
+++ films_with_nick_wahlberg.sql
@@ -1,11 +1,10 @@
-SELECT film.title, actor.first_name, actor.last_name
+-- A customer walks in and is a huge fan of the actor "Nick Wahlberg" and wants to know which movies he is in.
+-- Get a list of all the movies "Nick Wahlber" has been in.
+SELECT film.title, actor.first_name, actor.last_name -- Query results show film title, first and last name of actor
 FROM film
 INNER JOIN film_actor
-ON film.film_id = film_actor.film_id
+ON film.film_id = film_actor.film_id -- Connect film title with actor_id
 INNER JOIN actor
-ON actor.actor_id = film_actor.actor_id
-WHERE first_name = 'Nick' AND
+ON actor.actor_id = film_actor.actor_id -- Connect actor_id with actor first and last name
+WHERE first_name = 'Nick' AND -- Results in only Nick Wahlberg films
 last_name = 'Wahlberg'
-
-
-
