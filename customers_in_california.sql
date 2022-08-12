8217391ef436dd3f3adced234567ed05059e696b
Jessicab11 <79180025+Jessicab11@users.noreply.github.com>
2022-08-12 17:03:56 -0400

    Add description at top of code

--- customers_in_california.sql
+++ customers_in_california.sql
@@ -1,3 +1,5 @@
+-- California sales tax laws have changed and we need to alert our customers to this through email.
+-- What are the emails of the customers who live in California?
 SELECT email, district
 FROM address
 INNER JOIN customer
