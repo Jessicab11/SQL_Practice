46d887a3764106e0f81d8557fd7b960352bced09
Jessicab11 <79180025+Jessicab11@users.noreply.github.com>
2022-08-12 17:42:49 -0400

    add description and notes

--- discount_based_on_sum_amount.sql
+++ discount_based_on_sum_amount.sql
@@ -1,7 +1,10 @@
-SELECT SUM(amount), email
+-- I'd like to send a personalized email to some of our recent customers and offere them a discount based on the value of their recent purchases.
+-- I want to try to get some new regular customers, but also offere a small reward for our current regulars.
+-- I need the full name, account number, and email addresses of all customers who made at least one purchase in the last 6 months.
+SELECT first_name, last_name, email, SUM(amount), payment.customer_id -- return info
 FROM payment
 INNER JOIN customer
-ON payment.customer_id = customer.customer_id
-WHERE payment_date BETWEEN '2006-12-14' AND '2007-05-14'
-GROUP BY payment.customer_id, email
-ORDER BY SUM(amount) DESC
+ON payment.customer_id = customer.customer_id -- connect full name to amount
+WHERE payment_date BETWEEN '2006-12-14' AND '2007-05-14' -- last six months
+GROUP BY payment.customer_id, email, first_name, last_name
+ORDER BY SUM(amount) DESC -- largest purchased amounts to lowest purchased amounts
