8e0e5697551ff01ac029a2943805293b146da568
Jessicab11 <79180025+Jessicab11@users.noreply.github.com>
2022-08-12 17:24:23 -0400

    added notes at top of code

--- highest_customer_id_name_E_address_lower_500.sql
+++ highest_customer_id_name_E_address_lower_500.sql
@@ -1,3 +1,4 @@
+-- What customer has the highest customer ID number whose name starts with 'E' and has an address ID lower than 500?
 SELECT first_name, last_name
 FROM customer
 WHERE first_name Like 'E%' 
