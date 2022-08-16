31314f1ad216ebde40955ffef97304d8335800e4
Jessicab11 <79180025+Jessicab11@users.noreply.github.com>
2022-08-15 20:12:46 -0400

    Update SELFJOIN.sql

--- SELFJOIN.sql
+++ SELFJOIN.sql
@@ -1,5 +1,7 @@
-SELECT f1.title, f2.title, f1.length
+-- Find all the pairs of films that have the same length.
+
+SELECT f1.title, f2.title, f1.length -- creates two columns of film names, and one of their common length
 FROM film AS f1
 JOIN film AS f2
-ON f1.film_id != f2.film_id
-AND f1.length = f2.length
+ON f1.film_id != f2.film_id -- doesn't return pair of the same title
+AND f1.length = f2.length -- same length
