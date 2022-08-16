fa591125c37d361cd1627ef10bf2f5c9d1a9d45f
Jessicab11 <79180025+Jessicab11@users.noreply.github.com>
2022-08-15 20:09:52 -0400

    Update and rename tenniscourtbookingsoncertaindate.sql to tenniscourt_bookings_on_certain_date.sql

--- tenniscourt_bookings_on_certain_date.sql
+++ tenniscourt_bookings_on_certain_date.sql
@@ -1,10 +1,11 @@
 -- How can you produce a list of the start times for bookings for
 -- tennis courts, for the date '2012-09-21'?
 -- Return a list of start time and facility name pairing, ordered by the time.
+
 SELECT starttime, facilities.name
 FROM cd.bookings
 INNER JOIN cd.facilities
 ON bookings.facid = facilities.facid
-WHERE starttime BETWEEN '2012-09-21' AND '2012-09-22'
-AND facilities.name LIKE '%Tennis Court%'
-ORDER BY starttime
+WHERE starttime BETWEEN '2012-09-21' AND '2012-09-22' -- return only bookings on this date
+AND facilities.name LIKE '%Tennis Court%' -- return only tennis courts
+ORDER BY starttime -- ordered by time
